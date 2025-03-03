1eedef25e32ccb01c39699c44af1c48bfe73b66b
therealpeppy <132091707+therealpyppy@users.noreply.github.com>
2025-03-02 21:51:27 -0600

    Create suggestionScript.js

---
+++ suggestionScript.js
@@ -0,0 +1,28 @@
+import games from "https://cdn.jsdelivr.net/gh/therealpyppy/schoolgamesite/games.js"
+
+const searchInput = document.getElementById('search');
+const suggestionsBox = document.getElementById('suggestions');
+
+const body = document.querySelector('body');
+const gameCount = document.createElement('p');
+gameCount.textContent = "Game Count: "+Object.keys(games).length
+body.appendChild(gameCount)
+
+searchInput.addEventListener('input', () => {
+	const query = searchInput.value.toLowerCase();
+	const matches = Object.keys(games).filter(gameName => gameName.toLowerCase().includes(query));
+	suggestionsBox.innerHTML = "";
+	if (matches.length > 0) {
+		suggestionsBox.style.display = 'block';
+		matches.forEach(match => {
+			const div = document.createElement('div');
+			div.textContent = match;
+			div.addEventListener('click', () => {
+				window.location.href = "games/"+games[match];
+			});
+			suggestionsBox.appendChild(div);
+		});
+	} else {
+		suggestionsBox.style.display = 'none';
+	}
+});
