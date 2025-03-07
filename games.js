ce312b51de34b4b9defd7b853be0b8f839f6d14d
therealpeppy <132091707+therealpyppy@users.noreply.github.com>
2025-03-06 23:01:36 -0600

    Update games.js

--- games.js
+++ games.js
@@ -94,6 +94,15 @@ const games = {
 	"Baldis Basics": "baldisbasics.html",
 	"Balls and Bricks": "ballsandbricks.html",
 	"Basketball Stars": "basketballstars.html",
+	"Raft Wars": "raftwars.html",
+	"Super Mario 64": "supermario64.html",
+	"Super Mario Bros": "supermariobros.html",
+	"Super Mario Flash": "supersmashflash.html",
+	"Super Mario Flash 2": "supersmashflash2.html",
+	"There Is No Game": "thereisnogame.html",
+	"Throw a Potato": "throwapotato.html",
+	"Throw a Potato Again": "throwapotatoagain.html",
+	"Time Shooter 2": "timeshooter2.html"
 	"Test": "testingGames.html"
 };
 export default games;
